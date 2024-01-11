eab3123027cd7080afcf69be4474c0c85c2fbc5d
Logan Coning <loganconing@Logans-MBP.woh.rr.com>
2024-01-11 18:42:17 -0500

    initial

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const sequelize = require('./database');
+const User = require('./User');
+
+sequelize.sync().then(() => console.log('db is ready'));
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/users', async (req, res) => {
+    await User.create(req.body);
+    res.send('user is inserted');
+});
+
+app.get('/users', async (req, res) => {
+    const users = await User.findAll();
+    res.send(users);
+});
+
+app.get('/users/:id', async (req, res) => {
+    const requestedId = req.params.id;
+    const user = await User.findOne({ where: { id: requestedId }});
+    res.send(user);
+});
+
+app.listen(3000, () => {
+    console.log('app is running');
+});
